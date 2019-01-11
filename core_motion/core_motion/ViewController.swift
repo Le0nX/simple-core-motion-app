d070a6d15a62c83e994b4e44b98498fd8a32963e
Le0nX <cloudmindone@gmail.com>
2019-01-11 15:26:04 +0300

    feature: added some lanbels

---
+++ core_motion/core_motion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  core_motion
+//
+//  Created by Denis Nefedov on 11/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
